5f06909e6487cd3e1e82d59674aa8f416cb1c9d1
aebyss <edinphi@gmail.com>
2022-10-30 21:15:39 +0100

    Time Ch02 Exercise

--- ch02/Time.java
+++ ch02/Time.java
@@ -3,7 +3,22 @@ package ch02;
 public class Time {
 
     public static void main(String[] args) {
-        System.out.println("Hi");
-        System.out.println("Hi");
+        int timeOfDayInSeconds = 24 * 60 * 60;
+
+        int hour = 13, minute = 58, second = 10;
+        int timeSinceMidnightInSeconds = hour * 3600 + minute * 60 +
+                second;
+        int timeLeftInSeconds = timeOfDayInSeconds - timeSinceMidnightInSeconds;
+
+        int startHour = 17, startMinute = 45, startSecond = 36;  //start time of this exercise
+        int startTimeSinceMidnightInSeconds = startHour + 3600 + startMinute * 60 + startSecond;
+
+        int timeInterval = timeSinceMidnightInSeconds - startTimeSinceMidnightInSeconds;  // this time i spent on this exercise
+
+        System.out.println("Time since midnight: " + timeSinceMidnightInSeconds + " seconds");
+        System.out.println("Time remaining today: " + timeLeftInSeconds + "seconds");
+        System.out.println("Percentage of day passed: " + (double)timeSinceMidnightInSeconds / (double)(24*60*60) + "%");
+        System.out.println("Elapsed time: " + timeInterval + "seconds");
+
     }
 }
